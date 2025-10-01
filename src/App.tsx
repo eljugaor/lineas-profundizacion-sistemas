c63943f3fa3ff9e9e54893fd7d1468e1088b7ef2
Miguel_Ramírez <migueljora326@gmail.com>
2025-10-01 18:29:12 -0500

    Crear archivo App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Logout from './components/Logout';
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
