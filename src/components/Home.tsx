8fa7dfd4acfed17f4b06abe488723ef1fd25d1cc
Miguel_Ramírez <migueljora326@gmail.com>
2025-10-01 18:29:17 -0500

    Crear el componente Home en src/components/Home.tsx

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div className="container mt-5">
+      <h1>Bienvenido a las líneas de profundización de sistemas</h1>
+      <p>Explora las diferentes áreas de aprendizaje de ingeniería en sistemas.</p>
+    </div>
+  );
+};
+
+export default Home;
